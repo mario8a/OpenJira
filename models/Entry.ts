0b5acb32405aa857a96e4f3de828386cab825034
Mario Ochoa Tovar <mario8ato@hotmail.com>
2022-10-01 20:07:19 -0500

    Models

---
+++ models/Entry.ts
@@ -0,0 +1,14 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { Entry } from "../interfaces";
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Number },
+  status: { type: String, enum: { values: ['pending', 'in-progress', 'finished'], message: '{VALUE} no es un esttado permitido' }}
+});
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', entrySchema);
+
+export default EntryModel;
